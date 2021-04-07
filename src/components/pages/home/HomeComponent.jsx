b9259f235fc6424f08ddbfdf32f5c1eeed6829de
Dat-Hoang-Vo <flyingpenguins96@gmail.com>
2021-04-06 20:32:07 -0700

    Added LineChart component for future use

--- src/components/pages/home/HomeComponent.jsx
+++ src/components/pages/home/HomeComponent.jsx
@@ -1,9 +1,15 @@
+import LineChart from './../global/charts/LineChart';
+
 const HomeComponent = () => {
    return ( 
       <div>
+         <div>
+            <LineChart data={[5, 8, 12]} />
+         </div>
+         
          Home Component
       </div>
     );
-}
+} 
  
 export default HomeComponent;
