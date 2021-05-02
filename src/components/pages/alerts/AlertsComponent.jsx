f6337475c8dde28ccea29a9924a60f8980ee49a4
Yiwen Qiu <qiuy24@uw.edu>
2021-05-01 22:36:35 -0700

    fix time picker

--- src/components/pages/alerts/AlertsComponent.jsx
+++ src/components/pages/alerts/AlertsComponent.jsx
@@ -1,8 +1,7 @@
 import Box from '@material-ui/core/Box';
 import Alert from './Alert';
 
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import { useState } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
@@ -11,8 +10,8 @@ import {
 } from '@material-ui/pickers';
 
 const AlertsComponent = () => {
-   const [selectedDateFrom, setSelectedDateFrom] = React.useState(new Date());
-   const [selectedDateTo, setSelectedDateTo] = React.useState(new Date());
+   const [selectedDateFrom, setSelectedDateFrom] = useState(new Date());
+   const [selectedDateTo, setSelectedDateTo] = useState(new Date());
 
    const handleDateChangeFrom = (date) => {
      setSelectedDateFrom(date);
@@ -25,51 +24,51 @@ const AlertsComponent = () => {
    return (
       <Box padding="3vh 10vh 3vh 10vh">
          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-            <KeyboardDatePicker
-               variant="inline"
-               format="MM/dd/yyyy"
-               margin="normal"
-               id="date-picker-inline"
-               label="Date From"
-               value={selectedDateFrom}
-               onChange={handleDateChangeFrom}
-            />
-            <KeyboardTimePicker
-               variant="inline"
-               margin="normal"
-               id="time-picker"
-               label="Time From"
-               value={selectedDateFrom}
-               onChange={handleDateChangeFrom}
-               KeyboardButtonProps={{
-                  'aria-label': 'change time',
-               }}
-            />
-            <KeyboardDatePicker
-               variant="inline"
-               format="MM/dd/yyyy"
-               margin="normal"
-               id="date-picker-inline"
-               label="Date To"
-               value={selectedDateTo}
-               onChange={handleDateChangeTo}
-               KeyboardButtonProps={{
-                  'aria-label': 'change date',
-               }}
-            />
-            <KeyboardTimePicker
-               variant="inline"
-               margin="normal"
-               id="time-picker"
-               label="Time To"
-               value={selectedDateTo}
-               onChange={handleDateChangeTo}
-               KeyboardButtonProps={{
-                  'aria-label': 'change time',
-               }}
-            />
-            </Grid>
+            <Box>
+               <KeyboardDatePicker
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-from"
+                  label="Date From"
+                  value={selectedDateFrom}
+                  onChange={handleDateChangeFrom}
+               />
+               <KeyboardTimePicker
+                  variant="inline"
+                  margin="normal"
+                  id="time-picker-from"
+                  label="Time From"
+                  value={selectedDateFrom}
+                  onChange={handleDateChangeFrom}
+                  KeyboardButtonProps={{
+                     'aria-label': 'change time',
+                  }}
+               />
+               <KeyboardDatePicker
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-to"
+                  label="Date To"
+                  value={selectedDateTo}
+                  onChange={handleDateChangeTo}
+                  KeyboardButtonProps={{
+                     'aria-label': 'change date',
+                  }}
+               />
+               <KeyboardTimePicker
+                  variant="inline"
+                  margin="normal"
+                  id="time-picker-to"
+                  label="Time To"
+                  value={selectedDateTo}
+                  onChange={handleDateChangeTo}
+                  KeyboardButtonProps={{
+                     'aria-label': 'change time',
+                  }}
+               />
+            </Box>
          </MuiPickersUtilsProvider>
          <Alert />
          <Alert />
