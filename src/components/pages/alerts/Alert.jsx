dd4fa6665c3c4abfe12b3749bb5d818a439ce3a5
Shandy Kim <shandyjkim@gmail.com>
2021-05-04 16:46:56 -0700

    Add hover on alert

--- src/components/pages/alerts/Alert.jsx
+++ src/components/pages/alerts/Alert.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import {makeStyles} from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
@@ -5,15 +7,20 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 
-// import BellIcon from '../../../assets/UI_component_svg/BellIcon';
+import BellIcon from '../../../assets/UI_component_svg/BellIcon';
 import BellIcon2 from '../../../assets/UI_component_svg/BellIcon2';
 import ExampleAlert from '../../../assets/uploaded_blueprints/alerts-floor-plan-icon.jpg';
 
 const useStyles = makeStyles({
    alertCards: {
+      border: '2px solid #FFFFFF',
       borderRadius: '5px',
       width: 'auto',
-      marginTop: '3vh'
+      marginTop: '3vh',
+      "&:hover": {
+         borderColor: '#A2DAD9',
+         backgroundColor: '#F4FFFF',
+      }
    },
    cardContent: {
       display: 'flex',
@@ -44,12 +51,18 @@ const useStyles = makeStyles({
 
 const Alert = () => {
    const classes = useStyles();
+   let [bellIcon, setBellIcon] = useState(<BellIcon2 />);
+
    return (
       <Card className={classes.alertCards}>
-         <CardContent className={classes.cardContent}>
+         <CardContent
+            onMouseOver={() => setBellIcon(<BellIcon />)}
+            onMouseOut={() => setBellIcon(<BellIcon2 />)}
+            className={classes.cardContent}
+         >
             <Box alignSelf="flex-start">
                <IconButton className={classes.bellIcon}>
-                  <BellIcon2 />
+                  {bellIcon}
                </IconButton>
             </Box>
             <Box className={classes.alertContent}>
